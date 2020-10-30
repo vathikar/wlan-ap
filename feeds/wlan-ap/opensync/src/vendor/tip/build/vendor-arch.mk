bd52723e481fffbc1e44e76519fb4e6a8548c31f
aparna.salian <aparna.salian@globaledgesoft.com>
2020-10-30 15:28:06 +0530

    Netifd kconfig moved to vendor layer. Updated names to follow OpenSync naming conventions.
    Signed-off-by: aparna.salian <aparna.salian@globaledgesoft.com>

--- feeds/wlan-ap/opensync/src/vendor/tip/build/vendor-arch.mk
+++ feeds/wlan-ap/opensync/src/vendor/tip/build/vendor-arch.mk
@@ -1,9 +1,10 @@
-OS_TARGETS +=tip
+OS_TARGETS +=TIP
 
-ifeq ($(TARGET),tip)
+ifeq ($(TARGET),TIP)
 PLATFORM=openwrt
 VENDOR=tip
 PLATFORM_DIR := platform/$(PLATFORM)
-KCONFIG_TARGET ?= $(PLATFORM_DIR)/kconfig/openwrt_generic
+VENDOR_DIR := vendor/$(VENDOR)
+KCONFIG_TARGET ?= $(VENDOR_DIR)/kconfig/targets/$(TARGET)
 ARCH_MK := $(PLATFORM_DIR)/build/$(PLATFORM).mk
 endif
